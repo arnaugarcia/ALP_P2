534f080f8f1a86cf509325c31e206514dceb05f6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-04-21 13:33:07 +0200

    Added stage 1 formula and fixed menu choices

--- main.m
+++ main.m
@@ -1,16 +1,17 @@
-%Andres Martinez
-%Sergi Vidal
-%Arnau Garcia
+% Andres Martinez
+% Sergi Vidal
+% Arnau Garcia
 
 % Ver MatrizDeIncidencia.jpg para entender el funcionamiento de las aristas
 % y los nodos
 
+A = [-1 0; 1 -1; 0 1];
 choice = menu('Choose a ciruit to resolve', '1', '2', '3');
 switch choice
-    case '1'
-        % Choise 1
-    case '2'
+    case 1
+        result = resistencia_graf(A, 2, 1);
+    case 2
         % Choise 2
-    case '3'
+    case 3
         % Choise 3
 end
