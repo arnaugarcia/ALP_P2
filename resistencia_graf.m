54513af2ce0819be82c1e81e5dd6965ccfd7c8fa
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-04-26 18:26:27 +0200

    Created the matrix b

--- resistencia_graf.m
+++ resistencia_graf.m
@@ -7,26 +7,29 @@ function [R, Volt, Iint] = resistencia_graf(D, r, Iext)
         % TODO: rows = size(r, 1)
         [rows, columns] = size(r);
         
-        D
-        
         sizeOrientedMatrix = size(D, 1);
         
-        Dt = transpose(D)
-        Id = eye(columns)
+        Dt = transpose(D);
+        Id = eye(columns);
         
         % Se multiplica el vector de resistencias por su identidad
         % .* por cada columna
-        rId = r .* Id
+        rId = r .* Id;
         
-        zerosMatrix = zeros(sizeOrientedMatrix)
+        zerosMatrix = zeros(sizeOrientedMatrix);
         
         % Creamos el vector para la columna de las resistencias
-        intensityVector = zeros(1, columns)
+        intensityVector = zeros(1, columns);
         voltsVector = zeros(1, sizeOrientedMatrix);
         
         voltsVector(1, 1) = 1;
-                        
-        A = [rId, Dt; D, zerosMatrix; intensityVector, voltsVector]
+       
+                                
+        A = [rId, Dt; D, zerosMatrix; intensityVector, voltsVector];
+        
+        b = [zeros(1, columns) -Iext zeros(size(D, 1) - 2, 1) Iext 0]
+        
+        result = b.'\A
         
         R = 0;
         Volt = 1;
