6c1c90e1da61cc43b42bc3ffb05f383842889285
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-04-26 19:08:29 +0200

    Fixed menu choise

--- main.m
+++ main.m
@@ -4,11 +4,9 @@
 
 % Ver MatrizDeIncidencia.jpg para entender el funcionamiento de las aristas
 % y los nodos
-
-% choice = menu('Choose a ciruit to resolve', '1', '2', '3');
-clear
-clc
-choice = 1;
+%clc
+%clear
+choice = menu('Choose a ciruit to resolve', 1, 2, 3);
 switch choice
     case 1
         % Matrix de incidencia del circuito 1
@@ -18,10 +16,11 @@ switch choice
         % Intensidad de corriente exterior del circuito
         Iext = 3;
         
-        result = resistencia_graf(D, r, Iext)
-       
+       result = resistencia_graf(D, r, Iext);
     case 2
-        % Choise 2
+        
     case 3
         % Choise 3
 end
+
+
--- resistencia_graf.m
+++ resistencia_graf.m
@@ -3,6 +3,7 @@
 % Arnau Garcia
 
 function [R, Volt, Iint] = resistencia_graf(D, r, Iext)
+
         % Getting the row size of the matrix
         % TODO: rows = size(r, 1)
         [rows, columns] = size(r);
@@ -25,14 +26,14 @@ function [R, Volt, Iint] = resistencia_graf(D, r, Iext)
         voltsVector(1, 1) = 1;
        
                                 
-        A = [rId, Dt; D, zerosMatrix; intensityVector, voltsVector];
+        A = [rId, Dt; D, zerosMatrix; intensityVector, voltsVector]
         
         b = [zeros(1, columns) -Iext zeros(size(D, 1) - 2, 1) Iext 0]
         
-        result = b.'\A
+        result = A\b.'
         
-        R = 0;
-        Volt = 1;
-        Iint = 0;
+        Volt = result(columns + 1: end, 1)
+        Iint = result(1:columns,1)
+        R = abs(result(end)) / Iext
         
 end
